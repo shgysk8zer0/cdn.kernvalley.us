306ba6d1a7802123f72f61c931bcac91e502a126
Chris Zuber <shgysk8zer0@gmail.com>
2019-02-07 10:45:19 -0800

    Create `<input is="file-input">`
    Has built-in methods for  getting file data via a variety of `FileReader` methods

---
+++ components/file-input.js
@@ -0,0 +1,72 @@
+export default class HTMLFileInputElement extends HTMLInputElement {
+	constructor() {
+		super();
+		this.type = 'file';
+
+		this.addEventListener('change', async () => {
+			this.dispatchEvent(new CustomEvent('filechange', {detail: await this.getFiles()}));
+		});
+	}
+
+	get as() {
+		return this.getAttribute('as') || 'data';
+	}
+
+	set as(as) {
+		this.setAttribute('as', as.toLowerCase());
+	}
+
+	async fileChange() {
+		return await new Promise(resolve => {
+			this.addEventListener('filechange', event => resolve(event.detail), {once: true});
+		});
+	}
+
+	async getFiles() {
+		const files = [];
+
+		for await (const file of this.filesGenerator()) {
+			files.push(file);
+		}
+
+		return files;
+	}
+
+	async *filesGenerator() {
+		const as = this.as.toLowerCase();
+
+		for await (const file of this.files) {
+			yield await new Promise((resolve, reject) => {
+				const reader = new FileReader();
+				reader.addEventListener('error', reject);
+				reader.addEventListener('loadend', event => {
+					resolve({
+						file,
+						data: event.target.result,
+						modified: new Date(file.lastModified),
+						as: this.as,
+					});
+				});
+
+				switch(as) {
+				case 'data':
+					reader.readAsDataURL(file);
+					break;
+				case 'text':
+					reader.readAsText(file);
+					break;
+				case 'binary':
+					reader.readAsBinaryString(file);
+					break;
+				case 'arraybuffer':
+					reader.readAsArrayBuffer(file);
+					break;
+				default:
+					throw new Error(`Invalid option for "as" attribute: ${as}`);
+				}
+			});
+		}
+	}
+}
+
+customElements.define('file-input', HTMLFileInputElement, {extends: 'input'});
