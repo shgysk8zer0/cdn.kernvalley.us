b07abb746e24c532f329988ea913e327154c6f4b
Chris Zuber <shgysk8zer0@gmail.com>
2020-05-24 11:24:20 -0700

    Allow passing URI & size to Spotify Player constructor
    Also makes better use of parent class methods

--- components/spotify/player.js
+++ components/spotify/player.js
@@ -9,14 +9,56 @@ const TYPES = [
 	'artist',
 ];
 
+function createPlayer(large = null) {
+	const iframe = document.createElement('iframe');
+	iframe.height = large === true ? 380 : 80;
+	iframe.width = 300;
+	iframe.setAttribute('allowtransparency', 'true');
+	iframe.allow = 'encrypted-media';
+	iframe.referrerPolicy = 'origin';
+	iframe.loading = 'lazy';
+
+	if ('sandbox' in iframe) {
+		iframe.sandbox.add('allow-scripts', 'allow-popups', 'allow-same-origin');
+	}
+
+	return iframe;
+}
+
+function parseURI(uri) {
+	if (typeof uri !== 'string') {
+		throw new Error('URI is not a string');
+	} else if (! uri.startsWith('spotify:')) {
+		throw new Error('URI is not a valid Spotify URI');
+	} else {
+		const [, type = '', id = ''] = uri.split(':');
+		if (! TYPES.includes(type)) {
+			throw new Error(`Unsupported type: ${type}`);
+		} else if (! /^[A-z\d]{22}$/.test(id)) {
+			throw new Error(`Invalid Spotify ID ${id}`);
+		} else {
+			return {type, id};
+		}
+	}
+}
+
 customElements.define('spotify-player', class HTMLSpotifyTrackElement extends CustomElement {
-	constructor() {
+	constructor(uri = null, large = null) {
 		super();
 		this.attachShadow({mode: 'open'});
+
 		this.getTemplate('./components/spotify/player.html').then(tmp => {
 			this.shadowRoot.append(tmp.cloneNode(true));
 			this.dispatchEvent(new Event('ready'));
 		});
+
+		if (typeof uri === 'string') {
+			this.uri = uri;
+		}
+
+		if (typeof large === 'boolean') {
+			this.large = large;
+		}
 	}
 
 	get large() {
@@ -42,37 +84,19 @@ customElements.define('spotify-player', class HTMLSpotifyTrackElement extends Cu
 	async attributeChangedCallback(name, oldValue, newValue) {
 		switch(name) {
 		case 'large':
-			this.ready.then(() => {
+			this.ready.then(async () => {
 				const size = newValue === null ? 80 : 380;
-				const slot = this.shadowRoot.querySelector('slot[name="player"]');
-				slot.assignedNodes().forEach(el => el.height = size);
+				const nodes = await this.getSlotted('player');
+				nodes.forEach(el => el.height = size);
 			});
 			break;
 
 		case 'uri':
 			this.ready.then(async () => {
-				const slot = this.shadowRoot.querySelector('slot[name="player"]');
-
 				if (typeof newValue === 'string' && newValue.startsWith('spotify:')) {
-					const [, type = '', id = ''] = newValue.split(':');
-					if (! TYPES.includes(type)) {
-						throw new Error(`Unsupported type: ${type}`);
-					} else if (! /^[A-z\d]{22}$/.test(id)) {
-						throw new Error(`Invalid Spotify ID ${id}`);
-					}
+					const {type, id} = parseURI(newValue);
 
-					const iframe = document.createElement('iframe');
-					iframe.height = this.large ? 380 : 80;
-					iframe.width = 300;
-					iframe.slot = 'player';
-					iframe.setAttribute('allowtransparency', 'true');
-					iframe.allow = 'encrypted-media';
-					iframe.referrerPolicy = 'origin';
-					iframe.loading = 'lazy';
-
-					if ('sandbox' in iframe) {
-						iframe.sandbox.add('allow-scripts', 'allow-popups', 'allow-same-origin');
-					}
+					const iframe = createPlayer(this.large);
 
 					iframe.addEventListener('load', async () => {
 						this.dispatchEvent(new CustomEvent('trackchange', {detail: {
@@ -83,12 +107,12 @@ customElements.define('spotify-player', class HTMLSpotifyTrackElement extends Cu
 
 					iframe.addEventListener('error', console.error, {once: true});
 
-					iframe.src = new URL(`${type}/${id}`, SPOTIFY);
+					iframe.src = new URL(`${type}/${id}`, SPOTIFY).href;
+
 					await loaded();
-					slot.assignedNodes().forEach(el => el.remove());
-					this.append(iframe);
+					this.setSlot('player', iframe);
 				} else {
-					slot.assignedNodes.forEach(el => el.remove());
+					this.clearSlot('player');
 				}
 			});
 			break;
