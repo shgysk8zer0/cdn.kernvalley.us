c141dc814ba2d5cc3129946824a859f846bc4a72
Chris Zuber <shgysk8zer0@gmail.com>
2021-01-14 14:25:27 -0800

    Update service worker to be an initializing function
    Pass in `worker` and `config`. Setting workers on `self` seemed to not work correctly.

--- service-worker.js
+++ service-worker.js
@@ -1,91 +1,93 @@
-/* global config: false */
 /* eslint-env serviceworker */
+/* eslint no-unused-vars: 0 */
 
-self.addEventListener('install', async event => {
-	event.waitUntil((async () => {
-		try {
-			for (const key of await caches.keys()) {
-				if (key !== 'user') {
-					await caches.delete(key);
-				}
-			}
-
-			const cache = await caches.open(config.version);
-			await cache.addAll([...config.stale || [], ...config.fresh || []]).catch(console.error);
-		} catch (err) {
-			console.error(err);
-		}
-	})());
-});
-
-self.addEventListener('activate', event => event.waitUntil(clients.claim()));
-
-self.addEventListener('fetch', event => {
-	if (event.request.method === 'GET') {
-		event.respondWith((async () => {
-			if (Array.isArray(config.stale) && config.stale.includes(event.request.url)) {
-				const cached = await caches.match(event.request);
-				if (cached instanceof Response) {
-					return cached;
-				} else {
-					const [resp, cache] = await Promise.all([
-						fetch(event.request),
-						caches.open(config.version),
-					]);
-
-					if (resp.ok) {
-						cache.put(event.request, resp.clone());
-					}
-
-					return resp;
-				}
-			} else if (Array.isArray(config.fresh) && config.fresh.includes(event.request.url)) {
-				if (navigator.onLine) {
-					const [resp, cache] = await Promise.all([
-						fetch(event.request),
-						caches.open(config.version),
-					]);
-
-					if (resp.ok) {
-						cache.put(event.request, resp.clone());
-					}
-					return resp;
-				} else {
-					return caches.match(event.request);
-				}
-			} else if (Array.isArray(config.allowed) && config.allowed.some(entry => (
-				entry instanceof RegExp
-					? entry.test(event.request.url)
-					: event.request.url.startsWith(entry)
-			))) {
-				const resp = await caches.match(event.request);
-
-				if (resp instanceof Response) {
-					return resp;
-				} else {
-					const resp = await fetch(event.request);
-
-					if (resp instanceof Response) {
-						const cpy = resp.clone();
-						caches.open(config.version).then(cache => cache.put(event.request, cpy));
-						return resp;
-					} else {
-						console.error(`Failed in request for ${event.request.url}`);
+function init(worker, config) {
+	worker.addEventListener('install', async event => {
+		event.waitUntil((async () => {
+			try {
+				for (const key of await caches.keys()) {
+					if (key !== 'user') {
+						await caches.delete(key);
 					}
 				}
-			} else {
-				return fetch(event.request);
+
+				const cache = await caches.open(config.version);
+				await cache.addAll([...config.stale || [], ...config.fresh || []]).catch(console.error);
+			} catch (err) {
+				console.error(err);
 			}
 		})());
-	}
-});
+	});
 
-self.addEventListener('push', async event => {
-	const data = event.data.json();
-	if (('notification' in data) && Array.isArray(data.notification) && Notification.permission === 'granted') {
+	worker.addEventListener('activate', event => event.waitUntil(clients.claim()));
 
-		this.registration.showNotification(...data.notification);
-	}
-});
+	worker.addEventListener('fetch', event => {
+		if (event.request.method === 'GET') {
+			event.respondWith((async () => {
+				if (Array.isArray(config.stale) && config.stale.includes(event.request.url)) {
+					const cached = await caches.match(event.request);
+					if (cached instanceof Response) {
+						return cached;
+					} else {
+						const [resp, cache] = await Promise.all([
+							fetch(event.request),
+							caches.open(config.version),
+						]);
 
-self.addEventListener('error', console.error);
+						if (resp.ok) {
+							cache.put(event.request, resp.clone());
+						}
+
+						return resp;
+					}
+				} else if (Array.isArray(config.fresh) && config.fresh.includes(event.request.url)) {
+					if (navigator.onLine) {
+						const [resp, cache] = await Promise.all([
+							fetch(event.request),
+							caches.open(config.version),
+						]);
+
+						if (resp.ok) {
+							cache.put(event.request, resp.clone());
+						}
+						return resp;
+					} else {
+						return caches.match(event.request);
+					}
+				} else if (Array.isArray(config.allowed) && config.allowed.some(entry => (
+					entry instanceof RegExp
+						? entry.test(event.request.url)
+						: event.request.url.startsWith(entry)
+				))) {
+					const resp = await caches.match(event.request);
+
+					if (resp instanceof Response) {
+						return resp;
+					} else {
+						const resp = await fetch(event.request);
+
+						if (resp instanceof Response) {
+							const cpy = resp.clone();
+							caches.open(config.version).then(cache => cache.put(event.request, cpy));
+							return resp;
+						} else {
+							console.error(`Failed in request for ${event.request.url}`);
+						}
+					}
+				} else {
+					return fetch(event.request);
+				}
+			})());
+		}
+	});
+
+	worker.addEventListener('push', async event => {
+		const data = event.data.json();
+		if (('notification' in data) && Array.isArray(data.notification) && Notification.permission === 'granted') {
+
+			this.registration.showNotification(...data.notification);
+		}
+	});
+
+	worker.addEventListener('error', console.error);
+}
