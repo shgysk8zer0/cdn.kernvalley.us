f113d81db8520acfa85d11edcd5728fb058fb0a6
Chris Zuber <shgysk8zer0@gmail.com>
2021-03-13 15:01:48 -0800

    Misc. e-commerce enhancements
    - `<payment-requet>` component
    - Basic validation of Card numbers and expiration dates in `<payment-request>`
    - Full `<shopping-cart>` and `<button is="checkout-button">` experience, using `PaymentRequest` API

--- components/shopping-cart.js
+++ components/shopping-cart.js
@@ -1,14 +1,41 @@
-import { ShoppingCart } from '../js/std-js/ShoppingCart.js';
+import { ShoppingCart, version } from '../js/std-js/ShoppingCart.js';
 import { registerCustomElement } from '../js/std-js/custom-elements.js';
 import { create, query, text } from '../js/std-js/dom.js';
+import { save } from '../js/std-js/filesystem.js';
+import './checkout-button.js';
+
+const protectedData = new WeakMap();
 
 const styles = {
+	container: {
+		'color': 'inherit',
+		'background-color': 'inherit',
+		'box-sizing': 'border-box',
+		'font-size': '18px',
+		'max-width': '95vw',
+		'max-height': '100%',
+		'width': '600px',
+	},
+	header: {
+		'display': 'flex',
+		'flex-direction': 'row',
+		'justify-content': 'space-evenly',
+		'padding': '8px',
+		'gap': '8px',
+	},
+	row: {
+		'display': 'flex',
+		'flex-direction': 'row',
+		'justify-content': 'space-evenly',
+		'padding': '8px',
+		'gap': '8px',
+	},
 	cells: {
 		'flex-basis': '25%',
 		'flex-grow': '1',
 		'flex-shrink': '0',
 		'cursor': 'cell',
-		'height': '1.2em',
+		'padding': '0.2em',
 		'box-sizing': 'border-box',
 	},
 };
@@ -16,11 +43,42 @@ const styles = {
 registerCustomElement('shopping-cart', class HTMLShoppingCartElement extends HTMLElement {
 	constructor() {
 		super();
-		this.attachShadow({ mode: 'open' });
+		protectedData.set(this, { shadow: this.attachShadow({ mode: 'open' }) });
 	}
 
-	async connectedCallback() {
-		const cart = new ShoppingCart();
+	connectedCallback() {
+		this.update().catch(console.error);
+	}
+
+	get store() {
+		if (this.hasAttribute('store')) {
+			return this.getAttribute('store');
+		} else {
+			return 'shopping';
+		}
+	}
+
+	set store(val) {
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('store', val);
+		}
+	}
+
+	get version() {
+		return parseInt(this.getAttribute('version')) || version;
+	}
+
+	set version(val) {
+		if (Number.isInteger(val) && val > 0) {
+			this.setAttribute('version', val.toString());
+		} else {
+			this.removeAttribute('version');
+		}
+	}
+
+	async update({ store, version } = this) {
+		const cart = new ShoppingCart(store, version);
+		const { shadow } = protectedData.get(this);
 		const items = await cart.items;
 		const total = items.reduce((sum, { price, quantity }) => sum + price * quantity, 0);
 
@@ -37,30 +95,16 @@ registerCustomElement('shopping-cart', class HTMLShoppingCartElement extends HTM
 		const container = create('div', {
 			part: ['product-container'],
 			classList: ['cart-list'],
-			styles: {
-				'color': 'inherit',
-				'background-color': 'inherit',
-				'box-sizing': 'border-box',
-				'overflow': 'auto',
-				'font-size': '18px',
-				'max-width': '100%',
-				'max-height': '100%',
-			},
+			styles: styles.container,
 			children: [
 				create('header', {
 					part: ['header'],
-					styles: {
-						'display': 'flex',
-						'flex-direction': 'row',
-						'justify-content': 'space-evenly',
-						'padding': '8px',
-						'gap': '8px',
-					},
+					styles: styles.header,
 					children: [
-						create('b', { text: 'Product Name', styles: styles.cells}),
+						create('b', { text: 'Name', styles: styles.cells}),
 						create('b', { text: 'Quantity', styles: styles.cells }),
-						create('b', { text: 'Total Cost', styles: styles.cells }),
-						create('b', { text: 'Delete Item', styles: styles.cells }),
+						create('b', { text: 'Total', styles: styles.cells }),
+						create('b', { text: 'Delete', styles: styles.cells }),
 					]
 				})
 			]
@@ -73,26 +117,25 @@ registerCustomElement('shopping-cart', class HTMLShoppingCartElement extends HTM
 				classList: even ? ['row', 'row-even'] : ['row', 'row-odd'],
 				dataset: { uuid },
 				part: ['row'],
-				styles: {
-					'display': 'flex',
-					'flex-direction': 'row',
-					'justify-content': 'space-evenly',
-					'padding': '8px',
-					'gap': '8px',
-				},
+				styles: styles.row,
 				children: [
 					create('span', {
 						text: name,
 						dataset: { field: 'name' },
 						part: ['product-name', 'cell'],
-						styles: styles.cells ,
+						styles: styles.cells,
 					}),
 					create('span', {
 						text: quantity.toString(),
 						dataset: { field: 'quantity', uuid, cost: price },
 						part: ['product-quantity', 'cell'],
 						attrs: { contenteditable: true, tabindex: '0' },
-						styles: styles.cells ,
+						classList: ['inline-block'],
+						styles: { ...styles.cells,
+							border: '1px solid currentColor',
+							'border-radius': '4px',
+							'cursor': 'text',
+						},
 						events: {
 							input: ({ target }) => {
 								const quantity = parseInt(target.textContent.replace(/\D/g, ''));
@@ -126,39 +169,48 @@ registerCustomElement('shopping-cart', class HTMLShoppingCartElement extends HTM
 							})
 						],
 					}),
-					create('button', {
-						text: 'X',
-						dataset: { uuid, productName: name },
-						classList: ['delete-btn'],
-						part: ['delete-row-btn'],
-						attrs: { type: 'button' },
-						styles: {
-							'background-color': '#dc3545',
-							'color': '#fafafa',
-							'border': 'none',
-							'border-radius': '6px',
-							'cursor': 'pointer',
-							'font-size': '16px',
-							'padding': '8px',
-						},
-						events: {
-							click: ({ target }) => {
-								const el = target.closest('.delete-btn');
+					create('span', {
+						styles: { ...styles.cells, 'display': 'inline-flex' },
+						part: ['cell'],
+						children: [
+							create('span', { styles: { 'flex-grow': '1' }}),
+							create('button', {
+								text: 'X',
+								dataset: { uuid, productName: name },
+								classList: ['delete-btn'],
+								part: ['delete-row-btn'],
+								attrs: { type: 'button' },
+								styles: {
+									'background-color': '#dc3545',
+									'color': '#fafafa',
+									'border': 'none',
+									'border-radius': '6px',
+									'cursor': 'pointer',
+									'font-size': '16px',
+									'padding': '8px',
+									'margin-left': 'auto',
+								},
+								events: {
+									click: ({ target }) => {
+										const el = target.closest('.delete-btn');
 
-								if (confirm(`Remove ${el.dataset.productName} from cart?`)) {
-									requestAnimationFrame(() => {
-										cart.deleteItem(el.dataset.uuid).then(async () => {
-											const container = target.closest('.cart-list');
-											target.closest('.row').remove();
-											await updateTotal(container);
-										}).catch(err => {
-											console.error(err);
-											alert(err);
-										});
-									});
+										if (confirm(`Remove ${el.dataset.productName} from cart?`)) {
+											requestAnimationFrame(() => {
+												cart.deleteItem(el.dataset.uuid).then(async () => {
+													const container = target.closest('.cart-list');
+													target.closest('.row').remove();
+													await updateTotal(container);
+												}).catch(err => {
+													console.error(err);
+													alert(err);
+												});
+											});
+										}
+									}
 								}
-							}
-						}
+							})
+
+						]
 					})
 				]
 			}));
@@ -174,33 +226,95 @@ registerCustomElement('shopping-cart', class HTMLShoppingCartElement extends HTM
 				document.createElement('br'),
 				create('slot', { attrs: { name: 'note' }}),
 				document.createElement('hr'),
-				// create('button', {
-				// 	text: 'Checkout',
-				// 	part: ['checkout', 'btn'],
-				// 	classList: ['checkout-btn'],
-				// 	is: 'checkout-btn',
-				// 	styles: {
-				// 		'background-color': '#2B80F0',
-				// 		'color': '#fafafa',
-				// 		'border': 'none',
-				// 		'border-radius': '6px',
-				// 		'cursor': 'pointer',
-				// 		'font-size': '18px',
-				// 		'font-weight': 'bold',
-				// 		'padding': '12px 16px',
-				// 	},
-				// 	events: {
-				// 		click: () => alert('Not yet supported. Sorry :('),
-				// 	},
-				// 	children: [
-				// 		create('slot', { name: 'checkout-icon' }),
-				// 	]
-				// }),
 				create('span', { text: ' ' }),
 				create('button', {
-					text: 'Empty Cart',
+					is: 'checkout-button',
+					part: ['checkout-btn', 'btn'],
+					classList: ['btn', 'checkout-btn'],
+					hidden: true,
+					children: [
+						create('slot', { name: 'checkout-label', text: 'Checkout' }),
+						create('slot', { name: 'checkout-icon' }),
+					],
+					styles: {
+						'background-color': '#007bff',
+						'color': '#fafafa',
+						'border': 'none',
+						'border-radius': '6px',
+						'cursor': 'pointer',
+						'font-size': '18px',
+						'font-weight': 'bold',
+						'padding': '12px 16px',
+					},
+					events: {
+						pay: async ({ detail: request }) => {
+							try {
+								const resp = await request.show();
+								resp.complete('success');
+
+								import('./notification/html-notification.js').then(async ({ HTMLNotificationElement }) => {
+									new HTMLNotificationElement('Payment Response', {
+										body: 'No data is sent. What would you like to do now?',
+										requireInteraction: true,
+										vibrate: [200, 0, 200],
+										actions: [{
+											title: 'Show me',
+											action: 'dialog',
+										}, {
+											title: 'Save JSON',
+											action: 'save',
+										}, {
+											title: 'Dismiss',
+											action: 'dismiss',
+										}],
+										data: JSON.stringify(resp, null, 4)
+									}).addEventListener('notificationclick', ({ target, action }) => {
+										switch(action) {
+											case 'dialog':
+												Promise.resolve(target.data).then(json => {
+													const dialog = create('dialog', {
+														children: [
+															create('pre', {
+																children: [
+																	create('code', { text: json }),
+																]
+															})
+														],
+														events: {
+															click: ({ target }) => target.closest('dialog').close(),
+															close: ({ target }) => target.remove(),
+														}
+													});
+													document.body.append(dialog);
+													dialog.showModal();
+												}).catch(console.error);
+												break;
+
+											case 'save':
+												save(new File([target.data], 'Response.json', { type: 'application/json' }));
+												break;
+
+											case 'dismiss':
+												target.close();
+												break;
+										}
+									});
+								});
+							} catch(err) {
+								console.error(err);
+							}
+
+						}
+					}
+				}),
+				create('span', { part: 'spacer' }),
+				create('button', {
 					attrs: { type: 'button' },
 					part: ['clear-btn', 'btn'],
+					children: [
+						create('slot', { name: 'empty-label', text: 'Empty Cart' }),
+						create('slot', { name: 'empty-icon' }),
+					],
 					styles: {
 						'background-color': '#dc3545',
 						'color': '#fafafa',
@@ -227,13 +341,122 @@ registerCustomElement('shopping-cart', class HTMLShoppingCartElement extends HTM
 							}
 						}
 					},
-					children: [
-						create('slot', { attrs: { name: 'clear-cart-icon' }}),
-					]
 				})
 			]
 		}));
 
-		this.shadowRoot.append(container);
+		shadow.replaceChildren(container);
+
+		customElements.whenDefined('checkout-button').then(() => {
+			const btn = shadow.querySelector('.checkout-btn');
+			const { requestPayerName, requestPayerEmail, requestPayerPhone,
+				shippingOptions, shippingType } = this;
+
+			requestAnimationFrame(() => {
+				btn.requestPayerName = requestPayerName;
+				btn.requestPayerEmail = requestPayerEmail;
+				btn.requestPayerPhone = requestPayerPhone;
+				btn.shippingOptions = shippingOptions;
+				btn.shippingType = shippingType;
+				btn.hidden = false;
+				btn.disabled = false;
+			});
+		});
+	}
+
+	get shippingOptions() {
+		const script = this.querySelector('script[type="application/json"]');
+
+		if (script instanceof HTMLScriptElement) {
+			return JSON.parse(script.text);
+		} else {
+			return undefined;
+		}
+	}
+
+	set shippingOptions(val) {
+		const script = this.querySelector('script[type="application/json"]');
+
+		if (!Array.isArray(val) || val.length === 0) {
+			if (script instanceof HTMLScriptElement) {
+				script.remove();
+			}
+		} else if (script instanceof HTMLScriptElement) {
+			script.textContent = JSON.stringify(val);
+		} else {
+			const dataScript = document.createElement('script');
+			dataScript.setAttribute('type', 'application/json');
+			dataScript.textContent = JSON.stringify(val);
+			this.prepend(dataScript);
+		}
+	}
+
+	get supportedMethods() {
+		return this.getAttribute('supportedmethods') || 'basic-card';
+	}
+
+	set supportedMethods(val) {
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('supportedmethods', val);
+		} else {
+			this.removeAttribute('supportedmethods');
+		}
+	}
+
+	get supportedNetworks() {
+		if (this.hasAttribute('supportednetworks')) {
+			return this.getAttribute('supportednetworks').split(' ');
+		} else {
+			return ['visa', 'mastercard', 'discover', 'amex'];
+		}
+	}
+
+	set supportedNetworks(val) {
+		if (Array.isArray(val) && val.length !== 0) {
+			this.setAttribute('supportednetworks', val.join(' '));
+		} else {
+			this.removeAttribute('supportednetworks');
+		}
+	}
+
+	get requestPayerName() {
+		return this.hasAttribute('requestpayername');
+	}
+
+	set requestPayerName(val) {
+		this.toggleAttribute('requestpayername', val);
+	}
+
+	get requestPayerEmail() {
+		return this.hasAttribute('requestpayeremail');
+	}
+
+	set requestPayerEmail(val) {
+		this.toggleAttribute('requestpayeremail', val);
+	}
+
+	get requestPayerPhone() {
+		return this.hasAttribute('requestpayerphone');
+	}
+
+	set requestPayerPhone(val) {
+		this.toggleAttribute('requestpayerphone', val);
+	}
+
+	get requestShipping() {
+		const shipping = this.shippingOptions;
+		return Array.isArray(shipping) && shipping.length !== 0;
+	}
+
+	get shippingType() {
+		return this.getAttribute('shippingtype') || 'shipping';
+	}
+
+	set shippingType(val) {
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('shippingtype', val);
+		} else {
+			this.removeAttribute('shippingtype');
+		}
 	}
 });
