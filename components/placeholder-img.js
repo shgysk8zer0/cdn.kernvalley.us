401ea341a55ced319eb1229316ff51d7db2a9dbc
Chris Zuber <shgysk8zer0@gmail.com>
2021-04-28 16:34:26 -0700

    Create placeholder-img.js

---
+++ components/placeholder-img.js
@@ -0,0 +1,114 @@
+const protectedData = new WeakMap();
+
+customElements.define('placeholder-img', class HTMLPlaceholderImageElement extends HTMLImageElement {
+	constructor({ width, height, color, backgroundColor, text, format } = {}) {
+		super();
+		if (Number.isInteger(width)) {
+			this.width = width;
+		}
+
+		if (Number.isInteger(height)) {
+			this.height = height;
+		}
+
+		if (typeof color === 'string') {
+			this.color = color;
+		}
+
+		if (typeof backgroundColor === 'string') {
+			this.backgroundColor = backgroundColor;
+		}
+
+		if (typeof text === 'string') {
+			this.text = text;
+		}
+
+		if (typeof format === 'string') {
+			this.format = format;
+		}
+	}
+
+	get backgroundColor() {
+		if (this.hasAttribute('backgroundcolor')) {
+			return this.getAttribute('backgroundcolor').toUpperCase().replace(/[^A-F0-9]/g, '');
+		} else {
+			return null;
+		}
+	}
+
+	set backgroundColor(val) {
+		if (typeof val === 'string' && /#?[0-9A-Fa-f]{6}/.test(val)) {
+			this.setAttribute('backgroundcolor', val);
+		} else {
+			this.removeAttribute('backgroundcolor');
+		}
+	}
+
+	get color() {
+		if (this.hasAttribute('color')) {
+			return this.getAttribute('color').toUpperCase().replace(/[^A-F0-9]/g, '');
+		} else {
+			return null;
+		}
+	}
+
+	set color(val) {
+		if (typeof val === 'string' && /#?[0-9A-Fa-f]{6}/.test(val)) {
+			this.setAttribute('color', val);
+		} else {
+			this.removeAttribute('color');
+		}
+	}
+
+	get format() {
+		return this.getAttribute('format') || 'webp';
+	}
+
+	set format(val) {
+		if (['webp', 'png', 'jpg', 'gif'].includes(val)) {
+			this.setAttribute('format', val);
+		} else {
+			this.removeAttribute('format');
+		}
+	}
+
+	get text() {
+		return this.getAttribute('text');
+	}
+
+	set text(val) {
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('text', val);
+		} else {
+			this.removeAttribute('text');
+		}
+	}
+
+	update() {
+		const { width = 640, height, backgroundColor, color, text, format } = this;
+		const size = [width, height].filter(p => typeof p === 'number').join('x');
+		const parts = [size, backgroundColor, color].filter(p => typeof p === 'string');
+		const url = new URL(`/${[...parts].join('/')}.${format}`, 'https://via.placeholder.com');
+
+		if (typeof text === 'string') {
+			url.searchParams.set('text', text);
+		}
+
+		this.src = url.href;
+	}
+
+	attributeChangedCallback() {
+		if (protectedData.has(this)) {
+			cancelAnimationFrame(protectedData.get(this));
+		}
+
+		protectedData.set(this, requestAnimationFrame(() => {
+			this.update();
+			protectedData.delete(this);
+		}));
+	}
+
+	static get observedAttributes() {
+		return ['width', 'height', 'color', 'backgroundcolor', 'text', 'format'];
+	}
+}, { extends: 'img' });
