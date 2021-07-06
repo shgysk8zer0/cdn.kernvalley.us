d3cd69ade4248a4e67bd83522cd3151809de0036
Chris Zuber <shgysk8zer0@gmail.com>
2021-07-05 17:28:00 -0700

    Fix loading of styles

--- components/krv/events.js
+++ components/krv/events.js
@@ -1,25 +1,35 @@
-import { text, attr } from '../../js/std-js/dom.js';
+import { text, attr, when } from '../../js/std-js/dom.js';
 import { getHTML, getJSON } from '../../js/std-js/http.js';
 import { registerCustomElement } from '../../js/std-js/custom-elements.js';
+import { loadStylesheet } from '../../js/std-js/loader.js';
 import { meta } from '../../import.meta.js';
 
 registerCustomElement('krv-events', class HTMLKRVEventsElement extends HTMLElement {
 	constructor() {
 		super();
-		this.attachShadow({ mode: 'open' });
+		const parent = this.attachShadow({ mode: 'open' });
+
+		Promise.all([
+			getHTML(new URL('./components/krv/events.html', meta.url).href),
+			loadStylesheet(new URL('./components/krv/events.css', meta.url).href, { parent }),
+		]).then(([frag]) => {
+			this.shadowRoot.append(frag);
+			this.dispatchEvent(new Event('ready'));
+		});
 	}
 
-	async connectedCallback() {
-		const [base, data] = await Promise.all([
-			getHTML(new URL('./components/krv/events.html', meta.url).href),
+	connectedCallback() {
+		this.render();
+	}
+
+	async render() {
+		const [data] = await Promise.all([
 			getJSON('https://events.kernvalley.us/events.json'),
+			when(this, 'ready'),
 		]);
 
-		const tmp = base.getElementById('event-template').content;
-
-		attr('link[rel="stylesheet"]', { href: new URL('./components/krv/events.css', meta.url).href }, { base });
-		this.shadowRoot.append(base);
-		const events = data.map(({ name, url: href, description, startDate, endDate, location }) => {
+		const tmp = this.shadowRoot.getElementById('event-template').content;
+		const events = data.splice(0, this.count).map(({ name, url: href, description, startDate, endDate, location }) => {
 			const base = tmp.cloneNode(true);
 			const container = document.createElement('div');
 			const start = new Date(startDate);
@@ -47,7 +57,7 @@ registerCustomElement('krv-events', class HTMLKRVEventsElement extends HTMLEleme
 			return container;
 		});
 
-		this.shadowRoot.append(...events);
+		this.shadowRoot.getElementById('events-list').replaceChildren(...events);
 	}
 
 	get theme() {
@@ -64,5 +74,36 @@ registerCustomElement('krv-events', class HTMLKRVEventsElement extends HTMLEleme
 		} else {
 			this.removeAttribute('theme');
 		}
+	}
+
+	get count() {
+		if (this.hasAttribute('count')) {
+			return parseInt(this.getAttribute('count')) || 5;
+		} else {
+			return 5;
+		}
+	}
+
+	set count(val) {
+		if (Number.isInteger(val) && val > 0) {
+			this.setAttribute('count', val);
+		} else {
+			this.removeAttribute('count');
+		}
+	}
+
+	attributeChangedCallback(name/*, oldval, newVal*/) {
+		switch(name) {
+			case 'count':
+				this.render();
+				break;
+
+			default:
+				console.error(`Unhandled attribute changed: ${name}`);
+		}
+	}
+
+	static get observedAttributes() {
+		return ['count'];
 	}
 });
