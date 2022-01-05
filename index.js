402a4efb07291ff973c095b1af71209395890c80
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-05 14:31:12 -0800

    Move JavaScript to external script

---
+++ index.js
@@ -0,0 +1,45 @@
+import './js/std-js/deprefixer.js';
+import './js/std-js/shims.js';
+import { polyfill as trustPolyfill } from './js/std-js/TrustedTypes.js';
+import { polyfill as locksPolyfill } from './js/std-js/LockManager.js';
+import { creatPolicy } from './js/std-js/trust.js';
+import { loadScript } from './js/std-js/loader.js';
+const modules = [
+	'./components/current-year.js',
+	'./components/github/user.js',
+	'./components/ad/block.js',
+	'./components/share-button.js',
+	'./components/window-controls.js',
+	'./components/share-to-button/share-to-button.js',
+	'./components/weather/current.js',
+	'./components/leaflet/map.js',
+	'./components/leaflet/marker.js',
+	'./components/app/list-button.js',
+	'./components/app/stores.js',
+	'./js/std-js/theme-cookie.js',
+];
+
+document.addEventListener('securitypolicyviolation', console.error);
+
+Promise.allSettled([
+	trustPolyfill(),
+	locksPolyfill(),
+]).then(async () => {
+	const { polyfill: sanitizerPolyfill } = await import(new URL('./js/std-js/Sanitizer.js', import.meta.url));
+	await sanitizerPolyfill();
+	const sanitizer = new Sanitizer({...Sanitizer.getDefaultConfiguration(), allowCustomElements: true });
+	const policy = createPolicy('default', {
+		createHTML: input => sanitizer.sanitizeFor('div').innerHTML,
+		createScriptURL: input => {
+			if ([location.origin, 'https://cdn.kernvalley.us'].includes(new URL(input).origin)) {
+				return input;
+			} else {
+				throw new DOMException('Untrusted script src');
+			}
+		}
+	});
+
+	await Promise.allSettled(
+		modules.map(src => loadScript(policy.createScriptURL(src), { type: 'module' }))
+	).then(console.info);
+});
