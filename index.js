0a36645f839425ad3623ab55dfd6c7575235307e
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-05 15:17:16 -0800

    Move Service Worker registration

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import './js/std-js/deprefixer.js';
 import './js/std-js/shims.js';
 import { polyfill as trustPolyfill } from './js/std-js/TrustedTypes.js';
 import { polyfill as locksPolyfill } from './js/std-js/LockManager.js';
-import { creatPolicy } from './js/std-js/trust.js';
 import { loadScript } from './js/std-js/loader.js';
 const modules = [
 	'./components/current-year.js',
@@ -24,11 +23,11 @@ document.addEventListener('securitypolicyviolation', console.error);
 Promise.allSettled([
 	trustPolyfill(),
 	locksPolyfill(),
+	navigator.serviceWorker.register('/sw.js').catch(console.error),
 ]).then(async () => {
-	const { polyfill: sanitizerPolyfill } = await import(new URL('./js/std-js/Sanitizer.js', import.meta.url));
-	await sanitizerPolyfill();
+	const { Sanitizer } = await import(new URL('./js/std-js/Sanitizer.js', import.meta.url));
 	const sanitizer = new Sanitizer({...Sanitizer.getDefaultConfiguration(), allowCustomElements: true });
-	const policy = createPolicy('default', {
+	const policy = globalThis.trustedTypes.createPolicy('default', {
 		createHTML: input => sanitizer.sanitizeFor('div').innerHTML,
 		createScriptURL: input => {
 			if ([location.origin, 'https://cdn.kernvalley.us'].includes(new URL(input).origin)) {
@@ -40,6 +39,6 @@ Promise.allSettled([
 	});
 
 	await Promise.allSettled(
-		modules.map(src => loadScript(policy.createScriptURL(src), { type: 'module' }))
+		modules.map(src => loadScript(policy.createScriptURL(new URL(src, document.baseURI)), { type: 'module' }))
 	).then(console.info);
 });
