b774e1a149b9a9d81bffb96e86e3c204e82af3ac
Chris Zuber <shgysk8zer0@gmail.com>
2022-01-05 18:08:10 -0800

    More WIP implementing trust policy
    Figured out how to use Google Analytics, & Leaflet uses DOMPurify. Having issues with SVG in custom element templates.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import './js/std-js/shims.js';
 import { polyfill as locksPolyfill } from './js/std-js/LockManager.js';
 import { Sanitizer as SanitizerShim } from './js/std-js/Sanitizer.js';
 import { loadScript } from './js/std-js/loader.js';
-import { SanitizerConfig as config } from './js/std-js/SanitizerConfigBase.js';
-// import { enforce } from './js/std-js/trust-enforcer.js';
+import { SanitizerConfig as config } from './js/std-js/SanitizerConfig.js';
+import { enforce } from './js/std-js/trust-enforcer.js';
 import { createPolicy } from './js/std-js/trust.js';
 const modules = [
 	'./components/current-year.js',
@@ -22,36 +22,14 @@ const modules = [
 	'./js/std-js/theme-cookie.js',
 ];
 
-let shimmed = false;
 if (! ('Sanitizer' in globalThis && globalThis.Sanitizer.prototype.getConfiguration instanceof Function)) {
 	globalThis.Sanitizer = SanitizerShim;
-	shimmed = true;
 }
 
-const sanitizer = shimmed === false ? new globalThis.Sanitizer({
-	allowElements: [...Sanitizer.getDefaultConfiguration().allowElements, 'slot', 'svg', 'svg:*'],
-	allowAttributes: {...Sanitizer.getDefaultConfiguration().allowAttributes },
-	blockElements: Sanitizer.getDefaultConfiguration().blockElements,
-	dropAttributes: Sanitizer.getDefaultConfiguration().dropAttributes,
-	dropElements: Sanitizer.getDefaultConfiguration().dropElements,
-	allowCustomElements: true,
-}): new globalThis.Sanitizer({
-	allowElements: [...config.allowElements, 'slot', 'svg', 'svg:*'],
-	allowAttributes: config.allowAttributes,
-	blockElements: config.blockElements,
-	dropAttributes: config.dropAttributes,
-	dropElements: ['script'],
-	allowCustomElements: true,
-});
-console.log(sanitizer.getConfiguration());
+const sanitizer = new globalThis.Sanitizer(config);
+
 const policy = createPolicy('default', {
-	createHTML: input => {
-		const output = sanitizer.sanitizeFor('div', input).innerHTML;
-		if (input !== output) {
-			console.log({ input, output });
-		}
-		return output;
-	},
+	createHTML: input => sanitizer.sanitizeFor('div', input).innerHTML,
 	createScript: input => {
 		throw new DOMException(`Untrusted attempt to create script: "${input}"`);
 	},
@@ -64,17 +42,10 @@ const policy = createPolicy('default', {
 	}
 });
 
-console.log({ policy });
+enforce([policy.name, 'sanitize#html', 'empty#html', 'empty#script', 'dompurify']);
 Promise.allSettled([
 	// trustPolyfill(),
 	locksPolyfill(),
-	navigator.serviceWorker.register('/sw.js').catch(console.error),
-]).then(async () => {
-	// const { Sanitizer } = await import(new URL('./js/std-js/Sanitizer.js', import.meta.url));
-
-	// enforce([policy.name, 'sanitize#html', 'empty#html', 'empty#script']);
-
-	await Promise.allSettled(
-		modules.map(src => loadScript(policy.createScriptURL(new URL(src, document.baseURI)), { type: 'module' }))
-	).then(console.info);
-});
+	navigator.serviceWorker.register(policy.createScriptURL('/sw.js')).catch(console.error),
+	...modules.map(src => loadScript(policy.createScriptURL(new URL(src, document.baseURI)), { type: 'module' }))
+]);
