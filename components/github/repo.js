27caf8bc879b7f9fe547312bf32c95f059af0b56
Chris Zuber <shgysk8zer0@gmail.com>
2022-03-08 19:25:30 -0800

    Update `<github-repo>`
    Finally getting around to this...

--- components/github/repo.js
+++ components/github/repo.js
@@ -1,14 +1,231 @@
-import HTMLCustomElement from '../custom-element.js';
-import { $ } from '../../js/std-js/functions.js';
+import { text, attr, hide, unhide } from '../../js/std-js/dom.js';
+import { getHTML } from '../../js/std-js/http.js';
+import { loadStylesheet, loadImage } from '../../js/std-js/loader.js';
+import { purify as policy } from '../../js/std-js/htmlpurify.js';
+import { whenIntersecting } from '../../js/std-js/intersect.js';
+import { getDeferred } from '../../js/std-js/promises.js';
+import { registerCustomElement } from '../../js/std-js/custom-elements.js';
+import { meta } from '../../import.meta.js';
 
-customElements.define('github-repo', class HTMLGitHubRepoElement extends HTMLCustomElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: 'open' });
-		this.getTemplate('./components/github/repo.html').then(tmp => {
-			this.shadowRoot.append(tmp);
-			this.dispatchEvent(new Event('ready'));
+
+const symbols = {
+	shadow: Symbol('shadow'),
+	timeout: Symbol('timeout'),
+	controller: Symbol('controller'),
+};
+
+const ENDPOINT =  'https://api.github.com/repos/';
+const { resolve, promise: def } = getDeferred();
+const templatePromise = def.then(() => getHTML(new URL('./components/github/repo.html', meta.url), { policy }));
+
+async function getTemplate() {
+	resolve();
+	const tmp = await templatePromise;
+	return tmp.cloneNode(true);
+}
+
+async function getJSON(url, { signal } = {}) {
+	const key = `github-repo:${url}`;
+
+	if (signal instanceof AbortSignal && signal.aborted) {
+		throw signal.reason;
+	} else if (key in sessionStorage) {
+		const json = sessionStorage.getItem(key);
+		return JSON.parse(json);
+	} else {
+		const resp = await fetch(url, {
+			headers: new Headers({ Accept: 'application/json' }),
+			mode: 'cors',
+			signal,
 		});
+
+		if (resp.ok) {
+			resp.clone().text().then(json => sessionStorage.setItem(key, json));
+		}
+		return resp.json();
+	}
+}
+
+registerCustomElement('github-repo', class HTMLGitHubRepoElement extends HTMLElement {
+	constructor({ user, repo } = {}) {
+		super();
+
+		Object.defineProperties(this, {
+			[symbols.shadow]: {
+				enumerable: false,
+				configurable: false,
+				writable: false,
+				value: this.attachShadow({ mode: 'closed' }),
+			},
+			[symbols.timeout]: {
+				enumerable: false,
+				configurable: false,
+				writable: true,
+				value: NaN,
+			},
+			[symbols.controller]: {
+				enumerable: false,
+				configurable: false,
+				writable: true,
+				value: null,
+			}
+		});
+
+		if (typeof repo === 'string') {
+			this.repo = repo;
+		}
+
+		if (typeof user === 'string') {
+			this.user = user;
+		}
+	}
+
+	async attributeChangedCallback(name) {
+		await this.ready;
+		switch(name) {
+			case 'repo':
+			case 'user': {
+				const controller = new AbortController();
+				if (Number.isNaN(this[symbols.timeout])) {
+					clearTimeout(this[symbols.timeout]);
+
+					if (this[symbols.controller] instanceof AbortController && ! this[symbols.controller].aborted) {
+						this[symbols.controller.abort(new DOMException('Timeout'))];
+					}
+				}
+
+				this[symbols.controller] = controller;
+				this[symbols.timeout] = setTimeout(() => this.render({ signal: controller.signal }), 50);
+
+				break;
+			}
+
+			default:
+				throw new Error(`Unhandled attribute changed: "${name}"`);
+		}
+	}
+
+	async connectedCallback() {
+		if (this.loading === 'lazy') {
+			await whenIntersecting(this);
+		}
+
+		const tmp = await getTemplate();
+		await loadStylesheet(new URL('./components/github/repo.css', meta.url), { parent: this[symbols.shadow] });
+		this[symbols.shadow].append(tmp);
+		this.dispatchEvent(new Event('ready'));
+	}
+
+	async render({ signal } = {}) {
+		if (signal instanceof AbortSignal && signal.aborted) {
+			throw signal.reason;
+		} else {
+			await this.ready;
+			const base = this[symbols.shadow];
+
+			try {
+				const { repo, user } = this;
+
+				if (typeof user === 'string' && typeof repo === 'string') {
+					const {
+						name, description, homepage, html_url: url, has_issues: hasIssues, message: error,
+						open_issues: openIssues, license: { name: license, key: licenseId, licenseURL } = {},
+						owner: { login: username, avatar_url: image, html_url: profile  } = {},
+					} = await getJSON(new URL(`./${user}/${repo}`, ENDPOINT), { signal });
+
+					if (typeof error === 'string' && error.length !== 0) {
+						throw new Error(error);
+					}
+
+					text('[part~="name"]', name, { base });
+					hide('[part~="error"]', { base });
+					attr('[part~="repo-link"]', { href: url }, { base });
+					text('[part~="username"]',username, { base });
+
+					if (typeof description === 'string') {
+						unhide('[part~="description"]', { base });
+						text('[part~="description"]', description, { base });
+					} else {
+						hide('[part~="description"]', { base });
+					}
+
+					if (typeof homepage === 'string' && typeof licenseURL === 'string') {
+						text('[part~="homepage-text"]', new URL(homepage).hostname, { base });
+						attr('[part~="homepage-url"]', { href: homepage }, { base });
+						unhide('[part~="homepage"]', { base });
+					} else {
+						hide('[part~="homepage"]', { base });
+					}
+
+					if (typeof licenseId === 'string' && licenseId !== 'other') {
+						text('[part~="license-name"]', license, { base });
+						attr('[part~="license-url"]', {
+							href: new URL(`./${licenseId}/`, 'http://choosealicense.com/licenses/').href,
+						}, { base });
+						unhide('[part~="license"]', { base });
+					} else {
+						hide('[part~="license"]', { base });
+					}
+
+					if (typeof image === 'string') {
+						try {
+							const avatar = await loadImage(image, { height: 48, width: 48 });
+							base.querySelector('[part~="avatar"]').replaceChildren(avatar);
+							unhide('[part~="avatar"]', { base });
+						} catch(err) {
+							console.error(err);
+							hide('[part~="avatar"]', { base });
+						}
+					} else {
+						hide('[part~="avatar"]', { base });
+					}
+
+					if (hasIssues && Number.isSafeInteger(openIssues)) {
+						attr('[part~="issues-link"]', { href: new URL('./issues', `${url}/`).href }, { base }),
+						// This is sometimes off for unknown reasons
+						text('[part~="issue-count"]', openIssues.toString(), { base });
+						unhide('[part~="issues"]', { base });
+					} else {
+						hide('[part~="issues"]', { base });
+					}
+
+					attr('[part~="profile-link"]', { href: profile }, { base });
+					unhide('[part~="container"]', { base });
+				}
+			} catch(err) {
+				console.error(err);
+				text('[part~="error-message"]', err, { base });
+				hide ('[part~="container"]', { base });
+				unhide('[part~="error"]', { base });
+			} finally {
+				this[symbols.timeout] = NaN;
+				this[symbols.controller] = null;
+			}
+		}
+	}
+
+	get loading() {
+		return this.getAttribute('loading') || 'eager';
+	}
+
+	set loading(val) {
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('loading', val);
+		} else {
+			this.removeAttribute('loading');
+		}
+	}
+
+	get ready() {
+		const { resolve, promise } = getDeferred();
+
+		if (this.isConnected && this[symbols.shadow].childElementCount > 1) {
+			resolve();
+		} else {
+			this.addEventListener('ready', () => resolve(), { once: true });
+		}
+
+		return promise;
 	}
 
 	get repo() {
@@ -16,58 +233,22 @@ customElements.define('github-repo', class HTMLGitHubRepoElement extends HTMLCus
 	}
 
 	set repo(val) {
-		this.setAttribute('repo', val);
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('repo', val);
+		} else {
+			this.removeAttribute('repo');
+		}
 	}
 
-	async attributeChangedCallback(name) {
-		switch(name) {
-		case 'repo':
-			this.ready.then(async () => {
-				const { repo, shadowRoot } = this;
+	get user() {
+		return this.getAttribute('user');
+	}
 
-				if (typeof repo === 'string') {
-					const resp = await fetch(new URL(repo, 'https://api.github.com/repos/'), {
-						mode: 'cors',
-						headers: new Headers({
-							Accept: 'application/json',
-						}),
-					});
-
-					if (resp.ok) {
-						const data = await resp.json();
-						this.setSlot('data', JSON.stringify(data, null, 4));
-						this.setSlot('repo-link', data.full_name, {
-							tag: 'a',
-							attrs: {
-								href: data.html_url,
-							}
-						});
-						$('[part~="error"]', shadowRoot).hide();
-						$('[part~="name"]', shadowRoot).text(data.full_name);
-						$('[part~="repo-link"]', shadowRoot).attr({ href: data.html_url});
-						$('[part~="description"]', shadowRoot).text(data.description);
-						$('[part~="username"]', shadowRoot).text(data.owner.login);
-						$('[part~="avatar"]', shadowRoot).attr({src: data.owner.avatar_url});
-						$('[part~="user-profile"]', shadowRoot).attr({ href: data.owner.html_url});
-
-						if (data.has_issues) {
-							$('[part~="issues"]', shadowRoot).attr({
-								hidden: false,
-								href: `${data.html_url}/issues`,
-							});
-							$('[part~="issue-count"]', shadowRoot).text(data.open_issues);
-						}
-						$('[part~="container"]', shadowRoot).unhide();
-					} else {
-						const data = await resp.json();
-						$('[part~="error-link"]', shadowRoot).attr({ href: data.documentation_url });
-						$('[part~="error-message"]', shadowRoot).text(data.message);
-						$('[part~="container"]', shadowRoot).hide();
-						$('[part~="error"]', shadowRoot).unhide();
-					}
-
-				}
-			});
+	set user(val) {
+		if (typeof val === 'string' && val.length !== 0) {
+			this.setAttribute('user', val);
+		} else {
+			this.removeAttribute('user');
 		}
 	}
 
@@ -78,4 +259,3 @@ customElements.define('github-repo', class HTMLGitHubRepoElement extends HTMLCus
 		];
 	}
 });
-
