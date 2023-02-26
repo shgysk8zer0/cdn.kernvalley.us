73efad9cd1fa119ccc42430938ded5218b203e7f
Chris Zuber <shgysk8zer0@gmail.com>
2023-02-26 08:55:06 -0800

    Add default TrustedTypesPolicy

---
+++ js/default-trust-policy.js
@@ -0,0 +1,22 @@
+import { createPolicy } from './std-js/trust.js';
+import { SanitizerConfig as config } from './std-js/SanitizerConfig.js';
+
+const trustedOrigins = [
+	location.origin,
+	'https://cdn.kernvalley.us',
+	'https://unpkg.com',
+];
+
+export const sanitizer = new globalThis.Sanitizer(config);
+
+export const defaultPolicy = createPolicy('default', {
+	createHTML: input => sanitizer.sanitizeFor('div', input).innerHTML,
+	createScript: () => '',
+	createScriptURL: input => {
+		if (trustedOrigins.includes(new URL(input, document.baseURI).origin)) {
+			return input;
+		} else {
+			throw new DOMException(`Untrusted script src: <${input}>`);
+		}
+	}
+});
